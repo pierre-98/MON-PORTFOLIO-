a5a40547105caa25536facdb265bb354c19f834c
pierre <gkpatchapierre@gmail.com>
2025-06-18 15:47:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.querySelector(".contact-from").addEventListener("submit", function (e) 
   e.preventDefault();
   let prenom = document.getElementById("prenom").value.trim();
   let nom = document.getElementById("nom").value.trim();
-  let projet = document.getElementById("projet").value.trim();
+  let projet = document.getElementById("email").value.trim();
   let message = document.getElementById("message").value.trim();
 
-  if (!prenom || !nom || !projet || !message) {
+  if (!prenom || !nom || !email || !message) {
     alert("Merci de remplir tous les champs du formulaire !");
   } else {
     alert("Votre message a bien été envoyé !");
